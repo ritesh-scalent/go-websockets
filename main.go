7560846dcb8f8ee2a1af52592a6aa7a0ab5240b5
Everest1508 <riteshmahale15@gmail.com>
2024-09-06 15:00:14 +0530

    project structure fix

--- main.go
+++ main.go
@@ -1,42 +1,14 @@
 package main
 
 import (
-	"io"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	"github.com/ritesh-scalent/go-websockets/config"
+	httpEngine "github.com/ritesh-scalent/go-websockets/router/http"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/ws", func(c *gin.Context) {
-		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			return
-		}
-		defer conn.Close()
-		// scanner := bufio.NewScanner(conn.NetConn())
-		for {
-			messageType, r, err1 := conn.NextReader()
-			if err1 != nil {
-				return
-			}
-			w, _ := conn.NextWriter(messageType)
-			io.Copy(w, r)
-			w.Close()
+	enviroment := "dev"
+	configPath := "path"
+	config.Init(&enviroment, &configPath)
+	httpEngine.Run("8080")
 
-		}
-		// conn.WriteMessage(websocket.TextMessage, []byte("hello"))
-		// time.Sleep(time.Second)
-	})
-	router.Run(":8080")
 }
--- router/http/httpEngine.go
+++ router/http/httpEngine.go
@@ -1,9 +1,37 @@
-package http
+package httpEngine
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io"
 
-func Run() {
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func Run(port string) {
 	r := gin.Default()
+	r.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+		// scanner := bufio.NewScanner(conn.NetConn())
+		for {
+			messageType, r, err1 := conn.NextReader()
+			if err1 != nil {
+				return
+			}
+			w, _ := conn.NextWriter(messageType)
+			io.Copy(w, r)
+			w.Close()
 
-	r.Run()
+		}
+	})
+	r.Run(fmt.Sprintf(":%v", port))
 }
